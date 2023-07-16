fbb9a00be6406472eea086f24961d8ffd06cc379
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-07-16 16:01:42 +0900

    create api of buying bitcoin

--- main.go
+++ main.go
@@ -5,21 +5,21 @@ import (
 	"bitcoin/config"
 	"bitcoin/utils"
 	"fmt"
-	"time"
 )
 
 func main() {
 	utils.LoggingSetting(config.Config.Logfile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSercet)
 	
-	tickerChannel := make(chan bitflyer.Ticker)
-	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
-	for ticker := range tickerChannel {
-		fmt.Println(ticker)
-		fmt.Println(ticker.GetMidPrice())
-		fmt.Println(ticker.DateTime())
-		fmt.Println(ticker.TruncateDateTime(time.Second))
-		fmt.Println(ticker.TruncateDateTime(time.Minute))
-		fmt.Println(ticker.TruncateDateTime(time.Hour))
+	order := &bitflyer.Order{
+		ProductCode:     config.Config.ProductCode,
+		ChildOrderType:  "MARKET",
+		Side:            "BUY",
+		Size:            0.0001,
+		MinuteToExpires: 1,
+		TimeInForce:     "GTC",
 	}
+	res, _ := apiClient.SendOrder(order)
+	fmt.Println(res)
+
 }
