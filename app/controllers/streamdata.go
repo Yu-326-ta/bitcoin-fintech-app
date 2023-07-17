240e9ae384dd8956157899bc1b61ee010cebf616
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-07-17 19:10:17 +0900

    install google chart

--- app/controllers/streamdata.go
+++ app/controllers/streamdata.go
@@ -11,13 +11,15 @@ func StreamIngestionData() {
 	var tickerChannl = make(chan bitflyer.Ticker)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSercet)
 	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannl)
-	for ticker := range tickerChannl {
-		log.Printf("action=StreamIngestionData, %v", ticker)
-		for _, duration := range config.Config.Durations {
-			isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
-			if isCreated == true && duration == config.Config.TradeDuration {
+	go func() {
+	    for ticker := range tickerChannl {
+			log.Printf("action=StreamIngestionData, %v", ticker)
+			for _, duration := range config.Config.Durations {
+				isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
+				if isCreated == true && duration == config.Config.TradeDuration {
 				// TODO
+				}
 			}
 		}
-	}
+    }()
 }
--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"bitcoin/app/controllers"
-	"bitcoin/app/models"
 	"bitcoin/config"
 	"bitcoin/utils"
-	"fmt"
 )
 
 func main() {
 	utils.LoggingSetting(config.Config.Logfile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
-
+	controllers.StartWebServer()
 }
