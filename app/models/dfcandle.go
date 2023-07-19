c344d6d1db9136f27971ec467535cf3d651cdec6
Yu-326-ta <yoshinaga.yuta326@icloud.com>
2023-07-19 11:22:51 +0900

    create sma lines on chart

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -1,11 +1,21 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/markcheno/go-talib"
+)
 
 type DataFrameCandle struct {
 	ProductCode string        `json:"product_code"`
 	Duration    time.Duration `json:"duration"`
 	Candles     []Candle      `json:"candles"`
+	Smas        []Sma         `json:"smas,omitempty"`  
+}
+
+type Sma struct {
+	Period int `json:"period,omitempty"`
+	Values []float64 `json:"values,omitempty"`
 }
 
 // Candleのそれぞれのフィールドの値のみを返せるようにする（時間だけとかHihtだけとか）
@@ -55,4 +65,17 @@ func (df *DataFrameCandle) Volume() []float64 {
 		s[i] = candle.Volume
 	}
 	return s
+}
+
+func (df *DataFrameCandle) AddSma(period int) bool {
+	// キャンドルの数がピリオドより小さかったら計算できない
+	if len(df.Candles) > period {
+		// df.SmasにSmaの構造体を設定
+		df.Smas = append(df.Smas, Sma{
+			Period: period,
+			Values: talib.Sma(df.Closes(), period),
+		})
+		return true
+	}
+	return false
 }
